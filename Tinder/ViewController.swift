435cde73d3cb9930950189a7316edf0665394dd2
user144166 <user144166@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-12-01 21:44:03 -0500

    ViewDid error

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardImageView: UIImageView!
+    var cardInitialCenter: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cardImageView.image = #imageLiteral(resourceName: "ryan")
+        cardImageView.layer.cornerRadius = 10.0
+        
+    }
+    
+    
     @IBAction func Pangesture(_ sender: Any) {
-    
-    
-    
     
     
     }
     
+            
+            
+            
     var trayOriginalCenter: CGPoint!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,53 +38,90 @@ class ViewController: UIViewController {
     @IBOutlet var trayView: UIView!
     
     @IBAction func Swipecontrol(_ sender: UIPanGestureRecognizer) {
-        view.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
-        view.transform = view.transform.scaledBy(x: 0.5, y: 0.5)
-        
-        view.transform = view.transform.rotated(by:rotation)
-        
-        view.transform = view.transform.rotated(by angle: CGFloat(45 * M_PI / 180))
-        view.transform = CGAffineTransform(translationX: 50, y: 50)
-        
-        view.transform = CGAffineTransform(translationX: 50, y: 50)
-        view.transform = view.transform.translatedBy(x: 10, y: 10)
-        
-        view.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
-        view.transform = view.transform.rotated(by angle: CGFloat(45 * M_PI / 180))
-        
-        view.transform = CGAffineTransform.identity
-        
-        
-        
-        self.firstView.alpha = 0
-        self.secondView.alpha = 1
-        UIView.animate(withDuration:0.4, animations: {
-            // This causes first view to fade in and second view to fade out
-            self.firstView.alpha = 1
-            self.secondView.alpha = 0
-        }
-            
-            UIView.animate(withDuration: 0.3, animations: {
-                // Animate view properties here...
-            }) { (Bool) in
-                // Code to run after animation has finished...
-        }
-        
     }
-    UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
-    animations: { () -> Void in
-    self.trayView.center = self.trayDown
-    }, completion: nil)
     
-    cardView.transform = CGAffineTransformIdentity
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPanCard(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            print("Gesture began")
+            cardInitialCenter = cardImageView.center
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            var angle = 0.0
+            if (translation.x > 0) {
+                if (translation.x < 15) {
+                    angle = Double(translation.x)
+                } else {
+                    angle = 15.0
+                }
+                cardImageView.transform = CGAffineTransform(rotationAngle: CGFloat(angle * Double.pi / 180))
+                cardImageView.center = CGPoint(x: cardInitialCenter.x + translation.x, y:cardInitialCenter.y)
+            } else {
+                if (translation.x > -15) {
+                    angle = Double(translation.x)
+                } else {
+                    angle = -15.0
+                }
+                cardImageView.transform = CGAffineTransform(rotationAngle: CGFloat(angle * Double.pi / 180))
+                cardImageView.center = CGPoint(x: cardInitialCenter.x + translation.x, y:cardInitialCenter.y)
+            }
+        } else if sender.state == .ended {
+            print("Gesture ended")
+            
+            if (translation.x > 100) {
+                UIView.animate(withDuration:0.4, delay: 0.0,
+                               options: [],
+                               animations: { () -> Void in
+                                self.cardImageView.transform = CGAffineTransform(translationX: 500, y: 0)
+                }, completion: nil)
+            } else if (translation.x < -100) {
+                UIView.animate(withDuration:0.4, delay: 0.0,
+                               options: [],
+                               animations: { () -> Void in
+                                self.cardImageView.transform = CGAffineTransform(translationX: -500, y: 0)
+                }, completion: nil)
+            } else {
+                cardImageView.transform = CGAffineTransform.identity
+            }
+            
+        }
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationViewController = segue.destination as! ProfileViewController
+        destinationViewController.image = self.cardImageView.image
+    }
+    
+    
 }
 
 
-
-
-override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    var photoViewController = segue.destination as! PhotoViewController
+class ProfileViewController: UIViewController {
     
-    photoViewController.image = self.imageView.image
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    var image: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileImageView.layer.cornerRadius = 10.0
+        profileImageView.image = image
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
-
